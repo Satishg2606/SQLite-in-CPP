4723be3fa171ee72f6404d9e66750ccf892f88fa
Satish Gubge <gubgesatish@gmail.com>
2026-01-02 22:59:54 +0530

    Added constructed for Initializing Buffer

--- src/tasks/task1.cpp
+++ src/tasks/task1.cpp
@@ -10,23 +10,30 @@
 // Get the user input using getline function.
 // Check if the input is a meta-command (== .exit).-> if yes, then exit the program.
 
-struct {
+typedef struct {
     char* buffer;
-    size_t capacity;
-    size_t size;
-}IB;
+    size_t input_size;
+    size_t buffer_size;
+} InputBuffer;
 
+InputBuffer* new_input_buffer(){
+    InputBuffer* IB = new InputBuffer();
+    IB->buffer = nullptr;
+    IB->input_size = 0;
+    IB->buffer_size = 0;
+    return IB;
+}
 
 void print_promt(){
     std::cout<<"db > ";
 }
 
-void getline_input(){
+void getline_input(InputBuffer* IB){
     std::string line;
     std::getline(std::cin, line);
-    IB.buffer = new char[line.length() + 1];
-    std::strcpy(IB.buffer, line.c_str());
-    IB.size = line.length();
+    IB->buffer = new char[line.length() + 1];
+    std::strcpy(IB->buffer, line.c_str());
+    IB->input_size = line.length();
 }
 
 
@@ -34,10 +41,13 @@ int main(){
     while (true)
     {
         print_promt();
-        getline_input();
-        if(std::strcmp(IB.buffer, ".exit") == 0){
+        InputBuffer* IB = new_input_buffer();
+        getline_input(IB);
+        if(std::strcmp(IB->buffer, ".exit") == 0){
             exit(0);
+        }else{
+            std::cout<<"Unrecognized command '"<<IB->buffer<<"'"<<std::endl;
         }
     }
-    
+    return 0;
 }
